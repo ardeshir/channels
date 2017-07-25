a6c42676cba934b84d372c0c1301043a01c37371
ardeshir sepahsalar <ardeshir.org@gmail.com>
2017-07-25 15:14:33 +0000

    adding example

--- chan7/main.go
+++ chan7/main.go
@@ -3,18 +3,21 @@ import ( "fmt"
          "time" )
 var unbuffered = make(chan int)
 var   buffered = make(chan int, 5)
-
+// example code of buffered vs unbuffered channels, or 
+// sync or async channels
 func main() {
 	count := 5
         go func() {
              for i := 0; i <= count ; i++ {
                   fmt.Println("send message")
-                  unbuffered <- i
+                  // unbuffered <- i 
+                     buffered <- i
               }
          }()
        
        time.Sleep(time.Second * 3)
        for i := 0; i <= count; i++ {
-          fmt.Println(<- unbuffered)
+          // fmt.Println(<- unbuffered)
+          fmt.Println(<- buffered)
         }
 } // end main
